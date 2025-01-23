c950856044cf63456844a4fb6dc6d7bbf9af9555
Diana Avedisyan <diana-avedisyan@mail.ru>
2025-01-23 17:26:55 +0500

    Initial commit

---
+++ src/components/Menu.jsx
@@ -0,0 +1,35 @@
+import { NavLink } from 'react-router-dom';
+// проверить активный класс
+export default function Menu() {
+  return (
+    <nav className="menu">
+      <NavLink
+        className={({ isActive }) => isActive ? "menu__item menu__item-active" : "menu__item"}
+        to="/"
+      >
+        Главная
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => isActive ? "menu__item menu__item-active" : "menu__item"}
+        to="/drift"
+      >
+        Дрифт-такси
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => isActive ? "menu__item menu__item-active" : "menu__item"}
+        to="/timeattack"
+      >
+        Time Attack
+      </NavLink>
+
+      <NavLink
+        className={({ isActive }) => isActive ? "menu__item menu__item-active" : "menu__item"}
+        to="/forza"
+      >
+        Forza Karting
+      </NavLink>
+    </nav>
+  );
+}
